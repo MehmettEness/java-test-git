70b4a9a4a844b500eb9739df2657f1b6d7e68dff
Mehmet Enes <139991699+MehmettEness@users.noreply.github.com>
2023-11-25 19:33:35 +0300

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("deneme mesajı");
         System.out.println("deneme mesajı");
+        System.out.println("deneme mesajı2");
     }
 }
